a31ca7145387eb41eb3f220687e50c93ffc0ba85
yardmaister <130103544+yardmaister@users.noreply.github.com>
2023-05-10 01:34:41 +0200

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2020,
-                month: 7,
-                day: 17,
-                hours: 16,
+                year: 2023,
+                month: 6,
+                day: 10,
+                hours: 14,
                 minutes: 0,
                 seconds: 0,
                 words: {
